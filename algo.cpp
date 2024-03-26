61f448313c9f7af5de870b580b428eff2eceecc9
Gregor <gregor.poleg3g@gmail.com>
2024-03-26 13:33:11 +0100

    V main sem dodal if stavke

--- algo.cpp
+++ algo.cpp
@@ -6,11 +6,24 @@ using namespace std;
 
 // Counting sort za sortiranje po bitih
 void countingSort(vector<unsigned char>& arr, int exp) {
+
 }
 
 // Binarni Radix sort
 void binaryRadixSort(vector<unsigned char>& arr) {
+
 }
 
 int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        cerr << "Uporaba: " << argv[0] << " <vhodna_datoteka>" << endl;
+        return 1;
+    }
+
+    ifstream inputFile(argv[1]);
+    if (!inputFile) {
+        cerr << "Napaka pri odpiranju vhodne datoteke." << endl;
+        return 1;
+    }
+
 }
