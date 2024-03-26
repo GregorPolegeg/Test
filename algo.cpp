2f034210417c6a2b33f8fcdc389cd4b09f8cc495
Gregor <gregor.poleg3g@gmail.com>
2024-03-26 13:33:57 +0100

    Dokoncal sem main funkcijo

--- algo.cpp
+++ algo.cpp
@@ -26,4 +26,29 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
+    vector<unsigned char> numbers;
+    unsigned char num;
+    while (inputFile >> num)
+        numbers.push_back(num);
+    
+    inputFile.close();
+
+    // Sortiranje
+    binaryRadixSort(numbers);
+
+    // Izpis sortiranih števil v izhodno datoteko out.txt
+    ofstream outputFile("out.txt");
+    if (!outputFile) {
+        cerr << "Napaka pri ustvarjanju izhodne datoteke." << endl;
+        return 1;
+    }
+
+    for (int i = 0; i < numbers.size(); i++)
+        outputFile << static_cast<int>(numbers[i]) << " ";
+
+    outputFile.close();
+
+    cout << "Sortiranje uspešno zaključeno. Rezultat zapisan v out.txt." << endl;
+
+    return 0;
 }
