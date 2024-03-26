58c7b72e52c657ab43dfb24c61274a1e48d6efb0
Gregor <gregor.poleg3g@gmail.com>
2024-03-26 13:32:21 +0100

    Definiral sem funkcije v .cpp filu

---
+++ algo.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+// Counting sort za sortiranje po bitih
+void countingSort(vector<unsigned char>& arr, int exp) {
+}
+
+// Binarni Radix sort
+void binaryRadixSort(vector<unsigned char>& arr) {
+}
+
+int main(int argc, char* argv[]) {
+}
